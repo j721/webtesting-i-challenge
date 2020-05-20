649612dcbd261e868457ab695792f159265afeed
Julia Vu <juliadvu@gmail.com>
2020-05-20 18:14:32 -0500

    fixed false positive in fail function testing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -7,43 +7,43 @@ const item ={
     enhancement: 12
 }
 
-describe("repair function", ()=>{
-    it("a repair(item) method that should accept an object and return a new object with durability restored to 100", ()=>{
-        expect(enhancer.repair(item)).toEqual({...item, durability: 100})
-    })   
-})
+// describe("repair function", ()=>{
+//     it("a repair(item) method that should accept an object and return a new object with durability restored to 100", ()=>{
+//         expect(enhancer.repair(item)).toEqual({...item, durability: 100})
+//     })   
+// })
 
 
-describe("success function", ()=>{
-    it("enhancement should increase by 1 when enhancement level < 20",()=>{
-        expect(enhancer.succeed(item)).toEqual({...item, enhancement: item.enhancement + 1 })
-    })
-
-    it("enhancement will not increase if enhancement level = 20 and no changes to durability of item",()=>{
-        expect(enhancer.succeed({...item, enhancement: 20})).toEqual({...item, enhancement: 20})
-    })
-
-    it("durability will not decrease, if enhancement level changes to <2 0",()=>{
-        expect(enhancer.succeed({...item, durability: item.durability}))
-        .toEqual({...item, enhancement: item.enhancement + 1, durability: item.durability})
-    })
-})
-    
-
-// describe("fail function",()=>{
-//     it("if enhancement level < 15, the durability of item should decrease by 5 ",()=>{
-//         expect(enhancer.fail(item)).toEqual({...item, durability: item.durability - 5 })
+// describe("success function", ()=>{
+//     it("enhancement should increase by 1 when enhancement level < 20",()=>{
+//         expect(enhancer.succeed(item)).toEqual({...item, enhancement: item.enhancement + 1 })
 //     })
 
-//     it("if enhancement level >= 15, the durability of the item object should decrease by 10",()=>{
-//         expect(enhancer.fail({...item, enhancement: 15 }))
-//         .toEqual({...item,  enhancement: 15, durability: item.durability -10})
+//     it("enhancement will not increase if enhancement level = 20 and no changes to durability of item",()=>{
+//         expect(enhancer.succeed({...item, enhancement: 20})).toEqual({...item, enhancement: 20})
 //     })
 
-//     it("if enhancement level > 16, the enhancement level should decrease by 1, durability should decrease by 10" ,()=>{
-//         expect(enhancer.fail({...item, enhancement: 17})).toEqual({...item, enhancement: 16, durability: item.durability -10})
+//     it("durability will not decrease, if enhancement level changes to  value < 20",()=>{
+//         expect(enhancer.succeed({...item, durability: item.durability}))
+//         .toEqual({...item, enhancement: item.enhancement + 1, durability: item.durability})
 //     })
 // })
+    
+
+describe("fail function",()=>{
+    it("if enhancement level < 15, the durability of item should decrease by 5 ",()=>{
+        expect(enhancer.fail(item)).toEqual({...item, durability: item.durability - 5 })
+    })
+
+    it("if enhancement level >= 15, the durability of the item object should decrease by 10",()=>{
+        expect(enhancer.fail({...item, enhancement: 15 }))
+        .toEqual({...item,  enhancement: 15, durability: item.durability -10})
+    })
+
+    it("if enhancement level > = 16, the enhancement level should decrease by 1, durability should decrease by 10" ,()=>{
+        expect(enhancer.fail({...item, enhancement: 17})).toEqual({...item, enhancement: 16, durability: item.durability -10})
+    })
+})
 
 // describe("get function",()=>{
 //     it("if enhancement level = 0, name is not modified",()=>{
